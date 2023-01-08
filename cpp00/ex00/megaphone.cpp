fc010d229448a21ec60721172d19d08682d6e860
Nicole CHOW-YUEN-MIN <nchow-yu@e1r5p1.clusters.42paris.fr>
2023-01-09 00:00:39 +0100

    start cpp00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+int	main(int nb, char **argv)
+{
+	int		i;
+	size_t	j;
+
+	if (nb == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (i = 1; i < nb; i++)
+	{
+		for (j = 0; j < strlen(argv[i]); j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+		if (i < nb - 1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
